f640cc59c0716fe160bf4a7be3022d1cd93c168b
JoaoVitorOliveiraa <jvoliveira30@gmail.com>
2024-08-06 17:21:41 -0300

    Atualizações dos Botões e Implementação do Teste da Tela de Fim de Jogo.
    Mudanças:
    1 - Atualizações dos "png's" dos botões "Play Again" "Exit Game", "Back to Menu" e "Scoreboard".
    2 - Atualização da tela de fim de jogo, a fim de que fosse compatível com o formato de resolução 1280x720.
    3 - Adição do teste da tela de fim de jogo após clicar no botão "Play" do menu inicial do jogo.

--- window.py
+++ window.py
@@ -16,6 +16,7 @@ display = pygame.display.set_mode((LARGURA_TELA, ALTURA_TELA), pygame.SCALED | p
 menu_background = pygame.image.load(join('images', 'logo_redimensionado.png')).convert()
 ingame_background = pygame.image.load(join('images', 'universo.png'))
 credits_background = pygame.image.load(join('images', 'credits_bg.png'))
+game_over_background = pygame.image.load(join('images', 'game_over_screen_sem_botoes.png'))
 
 # Botões
 play_img = pygame.image.load(join('images', 'buttons', 'play.png')).convert_alpha()
@@ -23,12 +24,20 @@ options_img = pygame.image.load(join('images', 'buttons', 'options.png')).conver
 scoreboard_img = pygame.image.load(join('images', 'buttons', 'scoreboard.png')).convert_alpha()
 credits_img = pygame.image.load(join('images', 'buttons', 'credits.png')).convert_alpha()
 btm_img = pygame.image.load(join('images', 'buttons','btm_img.png')).convert_alpha()
+play_again_game_over_img = pygame.image.load(join('images', 'buttons','play_again.png')).convert_alpha()
+back_to_menu_game_over_img = pygame.image.load(join('images', 'buttons','back_to_menu.png')).convert_alpha()
+scoreboard_game_over_img = pygame.image.load(join('images', 'buttons','scoreboard_game_over.png')).convert_alpha()
+exit_game_game_over_img = pygame.image.load(join('images', 'buttons','exit_game.png')).convert_alpha()
 
 play_button = button.Button(261.93, 335.39, play_img, 0.8)
 scoreboard_button = button.Button(445.63, 382.39, scoreboard_img, 0.8)
 options_button = button.Button(714.67, 377.39, options_img, 0.8)
 credits_button = button.Button(898.37, 329.39, credits_img, 0.8)
 btm_button = button.Button(20, 20, btm_img, 1)
+play_again_game_over_button = button.Button(410, 370, play_again_game_over_img, 1)
+back_to_menu_game_over_button = button.Button(330, 295, back_to_menu_game_over_img, 1)
+scoreboard_game_over_button = button.Button(740, 290, scoreboard_game_over_img, 1)
+exit_game_game_over_button = button.Button(652, 370, exit_game_game_over_img, 1)
 
 running = True
 
@@ -65,12 +74,33 @@ while running:
             game_state = 'menu_credits'
 
     elif game_state == 'playing':
-        display.blit(ingame_background, (0, 0))
+        #display.blit(ingame_background, (0, 0)) Comentado com o propósito de testar a tela de fim de jogo
+        
+        # Pinta no fundo a tela de fim de jogo.
+        display.blit(game_over_background, (0, 0))
+
+        # Verifica se o botão de jogar novamente foi pressionado
+        if play_again_game_over_button.draw(display):
+            game_state == 'playing'
+        
+        # Verifica se o botão de voltar para o menu foi pressionado
+        elif back_to_menu_game_over_button.draw(display):
+            game_state = 'menu'
+        
+        # Verifica se o botão de mostrar o scoreboard foi pressionado
+        elif scoreboard_game_over_button.draw(display):
+            game_state = 'menu_scoreboard'
+        
+        # Verifica se o botão de sair do jogo foi pressionado
+        elif exit_game_game_over_button.draw(display):
+            pygame.quit()
+            exit()
 
     elif game_state == 'menu_credits':
         display.blit(credits_background, (0, 0))
         if btm_button.draw(display):
-            game_state ='menu'
+            game_state = 'menu'
+
     else:
         pass
 
